e74e80325daf55d89b5cc8ec11e2ce8fd6836bd6
Petr Novák <petr.novak73@seznam.cz>
2025-02-16 23:44:07 +0100

    zkouška

--- src/script.js
+++ src/script.js
@@ -8,3 +8,20 @@
         if (zmena) {zmena.style.backgroundColor = "#1B1A53"};
         if (zmena) {zmena.style.color = "yellow"};
     });
+
+
+/*document.addEventListener("DOMContentLoaded", function () {
+    const dialog = document.getElementById("myDialog");
+    const openButton = document.getElementById("openDialog");
+    const closeButton = document.getElementById("closeDialog");
+
+    openButton.addEventListener("click", function () {
+        dialog.showModal();
+        openButton.classList="invisible"; // Otevře dialog
+    });
+
+    closeButton.addEventListener("click", function () {
+        dialog.close();
+        openButton.classList="visible"; // Zavře dialog
+    });
+});*/
