92ccd7857078e5ee844c5fe1f81d68566ec5b40d
Petr Novák <petr.novak73@seznam.cz>
2025-02-17 12:57:31 +0100

    i se změnou barvy textu

--- src/script.js
+++ src/script.js
@@ -7,27 +7,36 @@
         
         input.addEventListener('change', function () {
             document.querySelectorAll('#chcolor').forEach(div => {
-                div.style.backgroundColor = ''; // Reset na výchozí
+                div.style.backgroundColor = ''; });
+            document.querySelectorAll('#popis').forEach(h3 => {
+                h3.style.color = ''; // Reset na výchozí                
             });
+            
+               
             const parentDiv = this.closest('#chcolor');
-
+            const h3 = parentDiv.querySelector('h3');
+            
+            
             if (this.checked) {
                 if (this.value === "activity") {
                     this.style.accentColor = "blue";
                     parentDiv.style.backgroundColor = "#e6f0ff";
-                    
+                    h3.style.color = "red";
+                                       
                 } else if (this.value === "mentions") {
                     this.style.accentColor = "blue";
                     parentDiv.style.backgroundColor = "#e6f0ff";
+                    h3.style.color = "red";
                     
                 } else if (this.value === "customised") {
                     this.style.accentColor = "blue";
                     parentDiv.style.backgroundColor = "#e6f0ff";
-                    
+                    h3.style.color = "red";                  
                 }
                 
+                
             }
-              
+             
         });
     });
 
