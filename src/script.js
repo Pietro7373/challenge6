01c1df238af448b6f7262129937b0a74d4b60453
Petr Novák <petr.novak73@seznam.cz>
2025-02-17 11:53:17 +0100

    konečně se povedlo

--- src/script.js
+++ src/script.js
@@ -3,25 +3,31 @@
 /*document.addEventListener("DOMContentLoaded", function () {
     const radioButtons = document.querySelectorAll("input[type='radio']");*/
 
-    document.querySelector("#activity").addEventListener("click", function(){
-        const zmena = document.querySelector("#chcolor");
-        if (zmena) {zmena.style.backgroundColor = "#1B1A53"};
-        if (zmena) {zmena.style.color = "yellow"};
+    document.querySelectorAll(".activity").forEach(input => {
+        
+        input.addEventListener('change', function () {
+            document.querySelectorAll('#chcolor').forEach(div => {
+                div.style.backgroundColor = ''; // Reset na výchozí
+            });
+            const parentDiv = this.closest('#chcolor');
+
+            if (this.checked) {
+                if (this.value === "activity") {
+                    this.style.accentColor = "blue";
+                    parentDiv.style.backgroundColor = "#e6f0ff";
+                    
+                } else if (this.value === "mentions") {
+                    this.style.accentColor = "blue";
+                    parentDiv.style.backgroundColor = "#e6f0ff";
+                    
+                } else if (this.value === "customised") {
+                    this.style.accentColor = "blue";
+                    parentDiv.style.backgroundColor = "#e6f0ff";
+                    
+                }
+                
+            }
+              
+        });
     });
 
-
-/*document.addEventListener("DOMContentLoaded", function () {
-    const dialog = document.getElementById("myDialog");
-    const openButton = document.getElementById("openDialog");
-    const closeButton = document.getElementById("closeDialog");
-
-    openButton.addEventListener("click", function () {
-        dialog.showModal();
-        openButton.classList="invisible"; // Otevře dialog
-    });
-
-    closeButton.addEventListener("click", function () {
-        dialog.close();
-        openButton.classList="visible"; // Zavře dialog
-    });
-});*/
